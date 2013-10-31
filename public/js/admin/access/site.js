253e0f639c451c9bf5204dceead9975e8acabbee
Felipe Neuhauss Araújo <felipe.neuhauss@gmail.com>
2013-10-31 09:50:35 -0200

    First commit

--- public/js/admin/access/site.js
+++ public/js/admin/access/site.js
@@ -1,5 +1,5 @@
 // Voli web
 var promise = Kinvey.init({
-    'appKey': 'kid_VPR1Vu_-J9',
-    'appSecret': '384c4ebeae2f49c7b02c1b1e865c6182'
+    'appKey': 'YOUR-APP-KEY',
+    'appSecret': 'YOUR-APP-SECRET'
 });
